e770fbbd215307408c1736967555cc2fddf29545
anton1113 <anton11131113@gmail.com>
2018-12-10 21:17:11 +0200

    Initial commit

---
+++ src/main/java/com/arash/aliases/AliasesApplication.java
@@ -0,0 +1,12 @@
+package com.arash.aliases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliasesApplication.class, args);
+    }
+}
